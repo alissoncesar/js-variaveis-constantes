66dedb5c30a2880e69dcea0477a6f419392a22af
alissoncesar <alissoncesar1113@gmail.com>
2024-12-05 12:02:49 -0300

    aula 1

---
+++ README.md
@@ -0,0 +1,9 @@
+# js-variaveis-constantes
+
+## erros comuns 
+
+- ReferenceErro: toda vez que você escrever algo que o javascript não encontrar;
+
+- SyntaxError: toda vez que você escrever código errado;
+
+- TypeError: toda vez que você errar algo relacionado ao tipo.
---
+++ index.js
@@ -0,0 +1,22 @@
+document.write(`<h1 class="titulo">Olá mundo!</h1>`);
+
+document.write(`<h1 class="titulo">Quanto é 2 + 2?</h1>`);
+let resultado = 2+2;
+document.write(`<h1 class="titulo">${resultado}</h1>`);
+document.write(`<h1 class="titulo">Adicione o dobro</h1>`);
+document.write(`<h1 class="titulo">${resultado + 8}</h1>`);
+
+let nome = "Lidiane";
+
+document.write(`<h1>Seja bem-vindo(a) ${nome}</h1>`)
+
+let habilitado = false;
+habilitado = true;
+
+document.write(`<h1>O ${nome} ${habilitado ? 'é' : 'não é'} habilitado</h1>`)
+
+const nomeDuplo = "camelCase";
+const nome_duplo = "snakeCase";
+const Pascalduplo = "pascalCase";
+const PascalDuplo = "PascalDuplo";
+
